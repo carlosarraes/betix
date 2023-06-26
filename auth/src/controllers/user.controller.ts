9ad55c39b6d0700e2b498a4ae5fe50d39aea382a
carraes <carraeshb@gmail.com>
2023-06-26 16:43:24 -0300

    feat: updates for cookie-session/jwt verification

--- auth/src/controllers/user.controller.ts
+++ auth/src/controllers/user.controller.ts
@@ -1,29 +1,65 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { SignupType } from '../types'
+import { BadRequest, type BodyType } from '../types'
 import { UserService } from '../services'
+import { createToken } from '../utils/token'
+import { Password } from '../utils/password'
 
 class User {
   constructor(private userService = new UserService()) {}
 
-  get = (_req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ message: 'get' })
+  get = (req: Request, res: Response) => {
+    res.status(StatusCodes.OK).send({ currentUser: req.currentUser || null })
   }
 
   signup = async (req: Request, res: Response) => {
-    const { email, password }: SignupType = req.body
+    const { email, password }: BodyType = req.body
 
     const user = await this.userService.create(email, password)
 
-    res.status(StatusCodes.OK).json(user)
+    const userJwt = createToken(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    )
+
+    req.session = {
+      jwt: userJwt,
+    }
+
+    res.status(StatusCodes.CREATED).json(user)
   }
 
-  signin = (_req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ message: 'signin' })
+  signin = async (req: Request, res: Response) => {
+    const { email, password }: BodyType = req.body
+
+    const user = await this.userService.find(email)
+
+    const passwordMatch = await Password.compare(user.password, password)
+
+    if (!passwordMatch) throw new BadRequest('Invalid credentials')
+
+    const userJwt = createToken(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    )
+
+    req.session = {
+      jwt: userJwt,
+    }
+
+    res.status(StatusCodes.OK).send(user)
   }
 
-  signout = (_req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ message: 'signout' })
+  signout = (req: Request, res: Response) => {
+    req.session = null
+
+    res.status(StatusCodes.NO_CONTENT).end()
   }
 }
 
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,11 +4,19 @@ import morgan from 'morgan'
 import { userRouter } from './routes'
 import { errorHandler } from './middleware'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.set('trust proxy', true)
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+)
 app.use(morgan('dev'))
 
 app.use('/api/users', userRouter)
@@ -16,6 +24,10 @@ app.use('/api/users', userRouter)
 app.use(errorHandler)
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined')
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
     console.log('Connected to MongoDB')
