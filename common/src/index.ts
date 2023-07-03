02e47365a29b5aa072da4734ed724b60d1366cb1
carraes <carraeshb@gmail.com>
2023-07-03 10:19:22 -0300

    feat: create support for NATS on common pkg

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,6 +7,12 @@ export * from "./middleware/requireAuth.middleware";
 export * from "./middleware/currentUser.middleware";
 
 export * from "./types/body.type";
+export * from "./types/user.interface";
+export * from "./types/subjects.type";
+export * from "./types/ticket.interface";
+
+export * from "./events/listenerClass";
+export * from "./events/publisherClass";
 
 export * from "./utils/token";
 export * from "./utils/password";
